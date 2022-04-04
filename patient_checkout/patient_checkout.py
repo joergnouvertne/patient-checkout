6dff1e152d41693d2d0a3b78de5c7793c735adb1
joerg <joerg.nouvertne@posteo.de>
2022-04-04 20:54:31 +0200

    Implement PatientCheckoutFunction and S3 event trigger

--- patient_checkout/patient_checkout.py
+++ patient_checkout/patient_checkout.py
@@ -5,10 +5,10 @@ s3 = boto3.client('s3')
 
 
 def lambda_handler(event, context):
-    bucket_name = event['Records']['0']['s3']['bucket']['name']
-    file_key = event['Records']['0']['s3']['object']['key']
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    file_key = event['Records'][0]['s3']['object']['key']
     obj = s3.get_object(Bucket=bucket_name, Key=file_key)
-    file_content = obj['body'].read().decode('utf-8')
+    file_content = obj['Body'].read().decode('utf-8')
     checkout_events = json.loads(file_content)
 
     for each_event in checkout_events:
