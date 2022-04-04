d9d5710a23515d4bc153a60feb122bd5ea3e9240
Joerg Nouvertne <joerg.nouvertne@posteo.de>
2022-04-04 21:20:34 +0200

    Merge pull request #1
    * Implement BillManagement Function

--- patient_checkout/patient_checkout.py
+++ patient_checkout/patient_checkout.py
@@ -1,10 +1,13 @@
 import boto3
 import json
+import os
 
 s3 = boto3.client('s3')
+sns_client = boto3.client('sns')
 
 
 def lambda_handler(event, context):
+    topic = os.environ.get('PATIENT_CHECKOUT_TOPIC')
     bucket_name = event['Records'][0]['s3']['bucket']['name']
     file_key = event['Records'][0]['s3']['object']['key']
     obj = s3.get_object(Bucket=bucket_name, Key=file_key)
@@ -13,3 +16,8 @@ def lambda_handler(event, context):
 
     for each_event in checkout_events:
         print(each_event)
+        sns_client.publish(
+            TopicArn=topic,
+            Message=json.dumps({'default': json.dumps(each_event)}),
+            MessageStructure='json'
+        )
